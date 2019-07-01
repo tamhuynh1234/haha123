3a5a542776d8162f9c9e284b7aa56d1ab72b1992
tamhuynh1234 <52303386+tamhuynh1234@users.noreply.github.com>
2019-07-01 10:58:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,45 @@
-<!DOCTYPE html>
 <html>
-<head>
-	<title>Hello</title>
-</head>
-<body>
-<?php echo "Hello I'm Tâm"; ?>
-</body>
+ <head>
+  <title>TUTI'sCloud</title>
+ </head>
+ <body>
+ 	<style type="text/css">
+ 		body{
+ 			background: #ADD8E6;
+ 		}
+ 		#content {
+ 			width: 100%;
+ 			margin: 0 auto;
+ 		}
+ 		.picture {
+ 			width: 70%;
+ 			float: right;
+ 		}
+ 		.menu {
+ 			width: 30%;
+ 			float: left;
+ 			border: 1px solid black;
+ 			
+ 			bottom: 100px;
+ 		}
+ 	</style>
+ <h1>HELLO EVERYBODY, THIS IS TUTI'S CLOUD APP</h1>
+ 
+ <div id="content">
+ 	<div class="picture">
+ 	<img src="https://www.google.com.vn/search?hl=vi&biw=1872&bih=1001&tbm=isch&sa=1&ei=T4QZXb28EsKroATppquwCw&q=h%C3%ACnh+n%E1%BB%81n&oq=h%C3%ACnh+n%E1%BB%81n&gs_l=img.3..0j0i67j0l8.13343.14514..15700...6.0..0.99.1166.14......0....1..gws-wiz-img.LgFerW_Pac4#imgrc=ES066tz3E8Xl9M:">
+ 	</div>
+ 	<div class="menu">
+ 	<h2>SOME DEMO FUNCTIONS:</h2>
+ 	<br>
+     <strong>VIEW DATABASE</strong><a href="ConnectToDB.php" target="_blank" > Click here</a>
+     <br/>
+     <strong>INSERT DATA TO THE DATABASE</strong><a href="InsertData.php" target="_blank" > Click here</a>
+     <br/>
+     <strong>UPDATE DATA TO THE DATABASE</strong><a href="UpdateData.php" target="_blank" > Click here</a> 
+     <br/>
+     <strong>DELETE DATA TO THE DATABASE</strong><a href="DeleteData.php" target="_blank" > Click here</a> 
+     </div>
+ </div>
+ </body>
 </html>
